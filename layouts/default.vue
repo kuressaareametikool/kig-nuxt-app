df94eaeb8592982f4e353e07c2af4921e4b8f6fa
TTuulik <101389598+TTuulik@users.noreply.github.com>
2022-10-03 14:14:41 +0300

    first changes

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,13 +1,12 @@
 <template>
-    <div class="bg-white min-h-screen flex flex-col">
-        <div class="flex gap-4 justify-around py-6 bg-amber-100">
-            <NuxtLink to="/" class="underline">Esimene</NuxtLink>
-            <NuxtLink to="/second" class="underline">Teine</NuxtLink>
-        </div>
-        <div class="p-4 flex-1 flex flex-col">
-            <slot></slot>
-        </div>
-
+  <div class="bg-white min-h-screen flex flex-col">
+    <div class="flex gap-4 justify-around py-6 bg-amber-100">
+      <NuxtLink to="/" class="underline">Esimene</NuxtLink>
+      <NuxtLink to="/second" class="underline">Teine</NuxtLink>
+      <NuxtLink to="/bla" class="underline">bla</NuxtLink>
     </div>
+    <div class="p-4 flex-1 flex flex-col">
+      <slot></slot>
+    </div>
+  </div>
 </template>
-
